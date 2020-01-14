05fc061c4b0cfefd9df63684261cd2265be19cfb
BaBeuloula <babeuloula@gmail.com>
2020-01-14 15:50:37 +0100

    tmp commit

---
+++ docker/install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+readonly DOCKER_PATH=$(dirname $(realpath $0))
+
+cd ${DOCKER_PATH};
+
+. ./lib/functions.sh
+
+check_requirements
+
+parse_env_docker
+
+docker-compose build --parallel
+if [[ -z "$(docker network ls -f name="${NETWORK}" --format "{{.Name}}")" ]]; then
+    docker network create "${NETWORK}"
+fi
+docker-compose stop
+docker-compose up -d
+
+block_success "Environment is started!"
