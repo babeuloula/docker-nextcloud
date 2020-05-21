00d03032b58256d7ac018f79e62ef0d5f298eb30
Jérémy REYNAUD <babeuloula@gmail.com>
2020-05-21 19:35:04 +0200

    ...

--- nextcloud/config/language.config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$CONFIG = [
-    'default_language' => getenv('NEXTCLOUD_LANGUAGE'),
-    'force_language' => getenv('NEXTCLOUD_LANGUAGE'),
-    'default_locale' => getenv('NEXTCLOUD_LOCALE'),
-    'force_locale' => getenv('NEXTCLOUD_LOCALE'),
-];
